41a02375d93845bf6873d7452425c37db3d64840
Rodolfo De Nadai <rdenadai@gmail.com>
2020-02-07 11:08:01 -0300

    Atualizações

--- resume/settings.py
+++ resume/settings.py
@@ -173,7 +173,7 @@ LOCALE_PATHS = (os.path.join(PROJECT_PATH, "locale"),)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # AWS CONNECTION
-USE_S3 = not DEBUG
+USE_S3 = False if DEBUG else True
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
