8dee70cc6492a53044f995d14eb5aaade6aff5e3
Rodolfo De Nadai <rdenadai@gmail.com>
2020-07-10 13:05:14 -0300

    wellknown path

--- resume/urls.py
+++ resume/urls.py
@@ -49,7 +49,7 @@ urlpatterns = [
     url(r"^admin/", admin.site.urls),
     url(r"^info/", include("info.urls")),
     url(r"^blog/", include("blog.urls")),
-    url(r".well-known/acme-challenge/test/", wellknown),
+    url(r".well-known/acme-challenge/<str:wellknown>", wellknown),
     url(
         r"^sitemap\.xml$",
         sitemap,
