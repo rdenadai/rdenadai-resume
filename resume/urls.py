1234fdf386d0dc8bfe79b2eca9a5c21f70d69330
Rodolfo De Nadai <rdenadai@gmail.com>
2020-07-10 12:52:34 -0300

    missing comma

--- resume/urls.py
+++ resume/urls.py
@@ -29,29 +29,44 @@ from .sitemaps import StaticViewSitemap
 from blog.models import Blog, Category
 
 blog_dict = {
-    'queryset': Blog.objects.all(),
-    'date_field': 'posted',
+    "queryset": Blog.objects.all(),
+    "date_field": "posted",
 }
 category_dict = {
-    'queryset': Category.objects.all(),
-    'date_field': 'posted',
+    "queryset": Category.objects.all(),
+    "date_field": "posted",
 }
 
-admin.site.site_header = 'rdenadai.com'
-handler403 = 'blog.views.view_400'
-handler403 = 'blog.views.view_403'
-handler404 = 'blog.views.view_404'
-handler500 = 'blog.views.view_500'
+admin.site.site_header = "rdenadai.com"
+handler403 = "blog.views.view_400"
+handler403 = "blog.views.view_403"
+handler404 = "blog.views.view_404"
+handler500 = "blog.views.view_500"
 
 
 urlpatterns = [
-    url(r'^$', blog_index, name="view_index"),
-    url(r'^admin/', admin.site.urls),
-    url(r'^info/', include('info.urls')),
-    url(r'^blog/', include('blog.urls')),
-    url(r'.well-known/acme-challenge/test/', )
-    url(r'^sitemap\.xml$', sitemap,
-        {'sitemaps': {'static': StaticViewSitemap, 'blog': GenericSitemap(blog_dict, priority=0.6), 'category': GenericSitemap(category_dict, priority=0.6)}},
-        name='django.contrib.sitemaps.views.sitemap'),
-    url(r'^robots\.txt$', TemplateView.as_view(template_name='resume/robots.txt', content_type='text/plain'), name="robots.txt"),
+    url(r"^$", blog_index, name="view_index"),
+    url(r"^admin/", admin.site.urls),
+    url(r"^info/", include("info.urls")),
+    url(r"^blog/", include("blog.urls")),
+    url(r".well-known/acme-challenge/test/",),
+    url(
+        r"^sitemap\.xml$",
+        sitemap,
+        {
+            "sitemaps": {
+                "static": StaticViewSitemap,
+                "blog": GenericSitemap(blog_dict, priority=0.6),
+                "category": GenericSitemap(category_dict, priority=0.6),
+            }
+        },
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
+    url(
+        r"^robots\.txt$",
+        TemplateView.as_view(
+            template_name="resume/robots.txt", content_type="text/plain"
+        ),
+        name="robots.txt",
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
