3f6a3bc107ad03d42a6f620390399b251e82268a
Rodolfo De Nadai <rdenadai@gmail.com>
2022-09-14 09:43:33 -0300

    chore: changed gunicorn config params;

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,5 @@ fi
 if [ "$MODE" = "development" ]; then
     pip install debugpy -t /tmp && python /tmp/debugpy --wait-for-client --listen 0.0.0.0:5678 -m manage runserver 0.0.0.0:8080
 elif [ "$MODE" = "production" ]; then
-    gunicorn resume.wsgi -b 0.0.0.0:8080 -w 2 -t 300 --access-logfile - --error-logfile -
+    gunicorn resume.wsgi -b 0.0.0.0:8080 -w 2 --threads=4 --worker-class=gthread -t 300 --access-logfile - --error-logfile -
 fi
